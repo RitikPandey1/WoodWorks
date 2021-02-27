89e89cdbecb3c7eee0a6c92d5484beb2b683c5b8
tjain1511 <tjain210@gmail.com>
2021-02-27 13:04:39 +0530

    setting up initial activities, fragments, layouts and resources

---
+++ Android App/WoodWorks/app/src/main/java/com/indianapp/woodworks/CartFrag.java
@@ -0,0 +1,25 @@
+package com.indianapp.woodworks;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CartFrag extends Fragment {
+    View rootView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView= inflater.inflate(R.layout.frag_cart,container,false);
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
